ed4692c349c36a2072a90f2c9d010e7d08ce02e8
garrik <federico.perotto@tiscali.it>
2024-11-06 12:05:18 +0100

    optimize finding of prime numbers using sieve of erathostenes
    math library required to use sqrt function

--- problem3/README.md
+++ problem3/README.md
@@ -5,7 +5,8 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143?
 
 ## Build
-gcc main.c primes.c -o main
+Math library needs to be linked using `-lm`, see `man sqrt`.
+`gcc main.c primes.c -lm -o main`
 
 ## Tests
-gcc test.c ../primes.c -o test -l cmocka
+`gcc test.c ../primes.c -o test -l cmocka -lm`
--- problem3/primes.c
+++ problem3/primes.c
@@ -1,13 +1,29 @@
+#include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
 
 #include "primes.h"
 
-int is_prime(long int n){
+// naive implementation that divides for every number lesser than n
+int is_prime_naive(long int n){
     if (n < 2) {
         return false;
     }
     for (long int i = 2; i < n; i += 1) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// optimized implementation using sieve of erathostenes
+// explanation https://math.stackexchange.com/questions/3875424/sieve-of-eratosthenes-why-can-we-stop-at-the-sqrt-n
+int is_prime(long int n){
+    if (n < 2) {
+        return false;
+    }
+    for (long int i = 2; i <= sqrt(n); i += 1) {
         if (n % i == 0) {
             return false;
         }

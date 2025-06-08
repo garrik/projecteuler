7594011f83b27c60a8a77b67775094bad6205f0f
garrik <federico.perotto@tiscali.it>
2025-06-08 03:10:13 +0200

    fix problem 4: solved with brute force

--- problem4/palindrome.c
+++ problem4/palindrome.c
@@ -1,30 +1,13 @@
 #include <assert.h>
+// #include <stdio.h>
 #include <string.h> // required by memset
 
 static const int DIGITS_SIZE = 50;
 
+int find_largest_palindrome_from_product_of(int n, int m);
 int is_palindrome(int number);
 
 int find_largest_palindrome_from_product_of(int n, int m) {
-    int i = n, j = m;
-    while (i > -1 && j > -1) {
-        int number = i * j;
-        if (is_palindrome(number)) {
-            // printf("Found palindrome %d, product of %d and %d\n", number, i, j);
-            return number;
-        }
-        // printf("Not a palindrome %d, product of %d and %d\n", number, i, j);
-        if (i < j) {
-            j--;
-        } 
-        else {
-            i--;
-        }
-    }
-    return -1;
-}
-
-int find_largest_palindrome_from_product_of___wrong(int n, int m) {
     for (int i = n; i > -1; i--) {
         for (int j = m; j > -1; j--) {
             int number = i * j;
